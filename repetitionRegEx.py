682081d55991070f0947af5dc0bf590e9c8f81af
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-10-10 14:03:21 +0530

    regex

---
+++ repetitionRegEx.py
@@ -0,0 +1,5 @@
+import re
+'''
+(x)? - x can appear 0 or 1 times to match the pattern
+
+'''
---
+++ x.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    vector<int> a;
+    a.push_back(1);
+    cout << a[0];
+    cout << "sanchi";
+    return 0;
+}
