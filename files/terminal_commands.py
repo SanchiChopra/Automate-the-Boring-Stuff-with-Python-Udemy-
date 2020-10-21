5fa5a39053ab1027f89af27974aa12726da19971
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-10-21 18:40:43 +0530

    change dir

---
+++ files/terminal_commands.py
@@ -0,0 +1,7 @@
+import os
+os.sep  # displays the separator: \\
+os.getcwd()  # returns the current working directory
+
+# change the current directory
+os.chdir('c:\\')
+os.getcwd()  # returns c:\\
